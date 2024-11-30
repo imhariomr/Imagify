178f7e88a8675209be8af65f2f944db8d3ca7a5c
imhariomr <imhariomr@gmail.com>
2024-11-30 23:34:11 +0530

    Fixed CORS issue and updated backend configuration

--- server/server.js
+++ server/server.js
@@ -6,10 +6,18 @@ const port = process.env.PORT || 4000;
 const connectDB = require('./config/mongodb');
 const {userRouter} = require('./routes/userRoute');
 const {imageRouter} = require('./routes/imageRoute');
+const allowedOrigins = ['https://imagify-frontend-vily.onrender.com'];
 
 
 dotenv.config();
-app.use(cors({ origin: '*' }));
+// app.use(cors({ origin: '*' }));
+app.use(cors({
+    origin: allowedOrigins,  // Allows only requests from this domain
+    methods: 'GET, POST, PUT, DELETE',  // Allows these HTTP methods
+    allowedHeaders: 'Content-Type, Authorization',  // Allows these headers
+  }));
+  app.options('*', cors());  // Handles preflight requests
+
 app.use(express.json());
 
 connectDB();
